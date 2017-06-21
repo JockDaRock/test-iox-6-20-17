2a10f0c629803bc4a57a6bc6088f87a0f2aeb234
JockDaRock <jock.da.rock@gmail.com>
2017-06-20 22:14:59 -0500

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd /usr/src/node-red && /usr/local/bin/npm start -- --dataDir /data
+cd /usr/src/node-red && /usr/local/bin/npm start
+#-- --dataDir /data
  
 while [ 1 ]; do
     sleep 1
